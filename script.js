39a4a820c21baef3a26ed32768a9db7e8d5f1c16
Mahmudul Hasan <mahmud.cse7@gmail.com>
2021-09-19 00:00:05 +0600

    added functionn

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let newTask = document.querySelector('#new-task');
 let form = document.querySelector('form');
 let todoUl = document.querySelector('#items');
-let completeUl = document.querySelector('.complete-list');
+let completeUl = document.querySelector('.complete-list ul');
+
+
+let createTask = function (task) {
+    let listItem = document.createElement('li');
+    let checkBox = document.createElement('input');
+}
