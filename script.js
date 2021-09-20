f6f1ba4f4a4cf069f356d89dfbb7f3bd58788f48
Mahmudul Hasan <mahmud.cse7@gmail.com>
2021-09-20 23:22:36 +0600

    added createTask function

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ let completeUl = document.querySelector('.complete-list ul');
 let createTask = function (task) {
     let listItem = document.createElement('li');
     let checkBox = document.createElement('input');
+    let label = document.createElement('label');
+
+    label.innerText = task;
+    checkBox.type = 'checkbox';
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+
+    return listItem;
+
 }
